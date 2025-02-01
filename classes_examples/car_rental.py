6e5f06f7e7d12fc0cfc498e8e075b7c01d6de248
A1iHassan <ali012wkout@gmail.com>
2025-02-01 16:26:21 +0200

    car_rental

--- classes_examples/car_rental.py
+++ classes_examples/car_rental.py
@@ -1,13 +1,16 @@
 class Car:
 
+    archive = {}
+
     # Constructor
-    def __init__(self, model):
+    def __init__(self, model, serial):
         self.model = model
+        self.serial = serial
         self.is_rented = False
 
     # Instance Methods
     def rent(self):
-        if not self.is_rented:
+        if not self.is_rented and Car.archive[self.serial]:
             self.is_rented = True
         else:
             print("Car is already rented.")
@@ -17,6 +20,11 @@ class Car:
             self.is_rented = False
         else:
             print("Car is not rented.")
+
+    @classmethod
+    def set_certificate(cls, car, certificate):
+        if not car.is_rented:
+            cls.archive[car.serial] = certificate
 
     @staticmethod
     def is_valid_model(model):
